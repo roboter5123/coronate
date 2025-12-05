e4ff2e5e98be08b798db7be0c486b8aa8ea8505f
John <john@johnridesa.bike>
2025-12-05 08:34:04 -0500

    Fix tests on Github Actions
    Works around this bug:
    https://github.com/jihchi/vite-plugin-rescript/issues/231

--- vite.config.js
+++ vite.config.js
@@ -5,36 +5,42 @@ import reScript from "@jihchi/vite-plugin-rescript";
 import { execSync } from "node:child_process";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      include: ["**/*.res.mjs"],
-    }),
+export default defineConfig(({ mode }) => {
+  let plugins = [
+    react({ include: ["**/*.res.mjs"] }),
     favicons("graphics/logo.svg"),
-    reScript({
-      loader: {
-        // output: './lib/bs',
-        suffix: ".res.mjs",
+  ];
+  // https://github.com/jihchi/vite-plugin-rescript/issues/231
+  if (!(mode === "test" && process.env.CI)) {
+    plugins.push(
+      reScript({
+        loader: {
+          // output: './lib/bs',
+          suffix: ".res.mjs",
+        },
+      }),
+    );
+  }
+  return {
+    plugins: plugins,
+    server: {
+      watch: {
+        // ignore ReScript build artifacts
+        ignored: ["**/lib/bs/**", "**/lib/ocaml/**", "**/lib/rescript.lock"],
       },
-    }),
-  ],
-  server: {
-    watch: {
-      // ignore ReScript build artifacts
-      ignored: ["**/lib/bs/**", "**/lib/ocaml/**", "**/lib/rescript.lock"],
+      port: 3000,
     },
-    port: 3000,
-  },
-  test: {
-    include: ["tests/**/*_test.res.mjs"],
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.js",
-  },
-  define: {
-    __LAST_COMMIT_DATE__: JSON.stringify(
-      execSync("git log -1 --format=%cs").toString().trim(),
-    ),
-    __IS_TEST__: JSON.stringify(process.env.VITEST),
-  },
+    test: {
+      include: ["tests/**/*_test.res.mjs"],
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./tests/setup.js",
+    },
+    define: {
+      __LAST_COMMIT_DATE__: JSON.stringify(
+        execSync("git log -1 --format=%cs").toString().trim(),
+      ),
+      __IS_TEST__: JSON.stringify(mode === "test"),
+    },
+  };
 });
